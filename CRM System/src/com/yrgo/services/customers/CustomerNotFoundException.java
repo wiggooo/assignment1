16df31bfa2bf11c62f9f8a6fb963a46bca8a82ce
wiggooo <152215345+wiggooo@users.noreply.github.com>
2024-11-04 18:38:17 +0100

    Delete CRM System directory

--- CRM System/src/com/yrgo/services/customers/CustomerNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package com.yrgo.services.customers;
-
-public class CustomerNotFoundException extends Exception {
-	// this is just to stop the annoying warning in Eclipse.
-	private static final long serialVersionUID = 1L;
-}
